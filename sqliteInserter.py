aaadd2badb061697c400ba8194cc95c335b53029
nbdy <me@nbdy.io>
2018-05-08 14:28:05 +0200

    progressbar not necessary

--- sqliteInserter.py
+++ sqliteInserter.py
@@ -1,6 +1,13 @@
 import sqlite3
 
-import progressbar
+try:
+    import progressbar
+
+    PROGRESSBAR_DISABLED = False
+except ImportError:
+    print "[W] pip install progressbar"
+    PROGRESSBAR_DISABLED = True
+
 from jinja2 import Template
 
 from helper import read_csv_2_list, listdir_execute, generate_keys
@@ -26,6 +33,15 @@ VALUES ({% for val in values %}'{{val}}'{% if loop.index < values|length %}, {% 
 """
 
 
+def process_row(row, c):
+    ka = []
+    kv = []
+    for k in row.keys():
+        ka.append(k)
+        kv.append(row.get(k).decode('utf-8').replace("'", ''))
+    c.execute(Template(INSERT).render(table=tbl, keys=generate_keys(ka), values=kv))
+
+
 def csv_2_sqlite(csv_file, db="oui.db"):
     rows = read_csv_2_list(csv_file)
     tbl = csv_file.split("/")[1].split(".")[0]
@@ -33,14 +49,14 @@ def csv_2_sqlite(csv_file, db="oui.db"):
     conn = sqlite3.connect(db)
     c = conn.cursor()
     c.execute(Template(CREATE_TABLE).render(table=tbl, keys=generate_keys(rows[0].keys())))
-    bar = progressbar.ProgressBar()
-    for row in bar(rows):
-        ka = []
-        kv = []
-        for k in row.keys():
-            ka.append(k)
-            kv.append(row.get(k).decode('utf-8').replace("'", ''))
-        c.execute(Template(INSERT).render(table=tbl, keys=generate_keys(ka), values=kv))
+    if not PROGRESSBAR_DISABLED:
+        bar = progressbar.ProgressBar()
+        for row in bar(rows):
+            process_row(row, c)
+    else:
+        for row in rows:
+            process_row(row, c)
+
     conn.commit()
     conn.close()
 
